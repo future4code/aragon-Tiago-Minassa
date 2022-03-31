0f350bdea223080941cc6e2b52484c9b5030555b
Tiago Castelo Bonfim Minassa <tiagocastelob@gmail.com>
2022-03-30 23:38:14 -0300

    Adicionando pasta lacos ao projeto git status
    git status
    ls

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,54 @@
+/* Exercicio de interpretação de código 
+Teste condole
+
+console.log("Bora estudaaaaaar!")
+ */
+
+/*
+//1. O comando retorna 5,
+// o for faz valor ser incrementado por '1' cada vez que o loop roda
+// o loop vai rodar até o 'i' ser 5
+let valor=0
+for (let i = 0; i < 5; i++) {
+    valor += 1
+}
+console.log(valor)
+
+
+//2a.Será impresso: 19,21,23,25,27,30
+// código provido:
+const lista=[10,11,12,15,18,19,21,23,25,27,30]
+for (let numero of lista) {
+   if (numero > 18){
+       console.log(numero)
+    }
+}
+//2b. não é suficiente, para acessar o índice, seria melhor usar:
+//for (let i=0; i<lista.length; i++) {console.log(lista[i])}
+
+
+
+//3. seria:
+*
+**
+*
+**
+
+
+Exercicio de escrita de código */
+
+let voltas = 0
+
+let quantidadeDeBichos= Number(prompt("Quantos bichinhos de estimação você tem?"))
+
+
+    while (quantidadeDeBichos = 0){
+        console.log("Que pena você pode adotar um bicho");
+    
+    }
+
+    while (quantidadeDeBichos > 0){
+     nomeDosBichos= prompt("Que legal, nos diga qual é o nome deles:")
+    }
+
+
