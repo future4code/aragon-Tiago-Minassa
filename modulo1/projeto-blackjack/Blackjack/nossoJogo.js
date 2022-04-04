ac20823a298379d9dcf3cab021614d863a1a8d8b
Tiago Castelo Bonfim Minassa <tiagocastelob@gmail.com>
2022-04-03 23:01:04 -0300

    Atualizando o projeto BlackJack

--- modulo1/projeto-blackjack/Blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/Blackjack/nossoJogo.js
@@ -12,17 +12,24 @@
  */
 console.log("Bem-vindo ao jogo!, Que a sorte esteja sempre ao seu favor!")
 
-if(confirm("Quer iniciar uma nova rodada?")) {
-} else { 
+if(confirm("Quer iniciar uma nova partida?")) {
+   let carta1Usuario = comprarCarta()
+   let carta2Usuario = comprarCarta()
+   let somaCartasUsuario = carta1Usuario.valor+carta2Usuario.valor
+   
+   let carta1Computador = comprarCarta()
+   let carta2Computador = comprarCarta()
+   let somaCartasComputador = carta1Computador.valor+carta2Computador.valor
+
+   console.log(`Cartas do Usuário: ${carta1Usuario.texto} e ${carta2Usuario.texto} = pontuação ${somaCartasUsuario}
+Cartas do Computador: ${carta1Computador.texto} e ${carta2Computador.texto} = pontuação ${somaCartasComputador}`)
+
+if(somaCartasUsuario===somaCartasComputador){console.log(`Empate!`)}
+   else if(somaCartasUsuario>21){console.log(`O computador ganhou!`)}
+   else if(somaCartasUsuario>somaCartasComputador){console.log(`O usuário ganhou!`)}
+   else if(somaCartasComputador>somaCartasUsuario){console.log(`O computador ganhou!`)}
+ else 
 	console.log( "O jogo acabou")
 }
 const carta = comprarCarta(); 
-
-
-const usuario = console.log(carta.texto) 
-// imprime o texto da carta. Nesse caso: "K♦️"
-
-console.log(carta.valor) 
-// imprime o valor da carta (um número). Nesse caso: 10)
-
 
