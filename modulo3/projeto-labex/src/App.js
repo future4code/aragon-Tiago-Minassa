1d6d1aca9a5f7daabdd2597842bb6f9ae2a0f968
Tiago Castelo Bonfim Minassa <tiagocastelob@gmail.com>
2022-05-24 23:09:27 -0300

    Adicionando a tentativa de segundo dia de implementação do LabeX (Obs. Quase tudo implementado, preciso continuar com o postman e localStorge mas ta indo

--- modulo3/projeto-labex/src/App.js
+++ modulo3/projeto-labex/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import HomePage from './pages/HomePage';
-import ErrorPage from './pages/ErrorPage';
-import AdminPage from './pages/AdminPage';
+import Router from './routes/Router';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    <Route path={"/"} element={<HomePage />}/>
-    <Route path={"*"} element={<ErrorPage />}/>
-    <Route path={"/adminPage"} element={<AdminPage />}/>
-    </Routes>
-    </BrowserRouter>
+    <Router />
     </>
   );
 }
--- modulo3/projeto-labex/src/pages/HomePage.js
+++ modulo3/projeto-labex/src/pages/HomePage.js
@@ -1,13 +1,43 @@
+import axios from "axios";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom"
-import Header from "../components/Header"
 
 function HomePage() {
     const navigate = useNavigate();
+    const [inputUsuario, setInputUsuario] = useState("")
+    const [inputSenha, setInputSenha] = useState ("")
 
-    return(
+
+    const handleInputUsuario = (event) => setInputUsuario(event.target.value)
+    const handleInputSenha = (event) => setInputSenha(event.target.value)
+
+    const login = () => {
+        const body = {
+            email: inputUsuario,
+            password: inputSenha 
+        }
+
+        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/:tiago/login",{
+          Headers: {
+              Authorizarion: "tiago-minassa-aragon"
+          }  
+        }, body).then(res => console.log(res.data))
+        .catch(error => console.log(error.reponse))
+    }
+
+    return (
         <main>
             <h1>LabeX</h1>
-            <button onClick={() => navigate("AdminPage") }>Entrar</button>
+            <br></br>
+            <label htmlFor="usuario"> Usuario:
+                <input onChange={handleInputUsuario} type="text" name="usuario" id="usuario" value={inputUsuario} ></input>
+            </label>
+            <br></br>
+            <label htmlFor="senha"> Senha: 
+                <input onChange={handleInputSenha} type="text" name="senha" id="senha" value={inputSenha}></input>
+            </label>
+            <br></br>
+            <button onClick={login}>Entrar</button>
             <hr></hr>
             <h2>Inscreva-se numa nova viagem!</h2>
             <hr></hr>
