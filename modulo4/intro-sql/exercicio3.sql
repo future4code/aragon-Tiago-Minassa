92e68b6ccf1d096f08be24c54e3765b23424f7b5
Tiago Castelo Bonfim Minassa <tiagocastelob@gmail.com>
2022-07-11 23:46:15 -0300

    enviando projeto de introdução ao SQL

---
+++ modulo4/intro-sql/exercicio3.sql
@@ -0,0 +1,16 @@
+// LETRA A
+SELECT * FROM `Headcount`;
+
+//LETRA B
+SELECT id AS identifier
+FROM `Headcount`;
+
+//LETRA C
+SELECT id FROM `Headcount`
+WHERE name="Laura";
+
+//LETRA D
+SELECT name FROM `Headcount`
+WHERE name LIKE '%a%';
+
+//LETRA E
