64138b8e23a4cdd08d32880507b7f4c6c730f534
Tiago Castelo Bonfim Minassa <tiagocastelob@gmail.com>
2022-08-08 23:55:22 -0300

    adicionando o exercicio de arquitetura em camadas

---
+++ modulo5/arquitetura-camadas/src/controller/UserController.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/UserBusiness";
+
+export class UserController {
+    public signup = async (req: Request, res: Response) => {
+        try {
+            const userBusiness = new UserBusiness()
+            const response = await userBusiness.signup({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            })
+
+            res.status(201).send(response)
+        } catch (error) {
+            res.status(400).send({ message: error.message })
+        }
+    }
+
+    public login = async (req: Request, res: Response) => {
+        try {
+            const userBusiness = new UserBusiness()
+            const response = await userBusiness.login({
+                email: req.body.email,
+                password: req.body.password
+            })
+
+            res.status(200).send(response)
+        } catch (error) {
+            res.status(400).send({ message: error.message })
+        }
+    }
+
+    public getUsers = async (req: Request, res: Response) => {
+        try {
+            const userBusiness = new UserBusiness()
+            const response = await userBusiness.getUsers({
+                token: req.headers.authorization,
+                name: req.query.name,
+                sort: req.query.sort,
+                limit: req.query.limit,
+                page: req.query.page
+            })
+
+            res.status(200).send(response)
+        } catch (error) {
+            res.status(400).send({ message: error.message })
+        }
+    }
+} 
